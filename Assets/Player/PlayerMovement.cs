f6c6d10be29e8e0b4560d5cc81bb8ca9129d349b
Janko Hu <hu.janko@yahoo.de>
2022-03-29 16:31:47 +0200

    feat 3: Anmerkungen umgesetzt

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -4,50 +4,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float moveSpeed = 1;//0.5f
-
-    public Rigidbody2D rb;
-    public Animator animator;
-
-    Vector2 movement;
-
-    float maxStamina = 250f;
-    float currentStamina = 250f;
-    public const float sprintStat = 2;
-    public const float walking = 1;//0.5f
-    // Update is called once per frame
+    private Rigidbody2D rb;
+    private Animator animator;
+    public Vector2 movement;
+    private float moveSpeed = 1;//0.5f
+    private float maxStamina = 250f;
+    private float currentStamina = 250f;
+    private const float sprintStat = 2;
+    private const float walking = 1;
+    
     void Update() {
-        //Input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude); //Optimized to length of a movement vector squared
-        
     }
 
     void FixedUpdate() {
-        //Movement
-        Debug.Log(currentStamina);
         if (Input.GetKey(KeyCode.LeftShift)) {
             if (currentStamina > 0) {
                 moveSpeed = sprintStat;
                 currentStamina--;
-            }
-            else {
+            } else {
                moveSpeed = walking; 
             } 
-        }
-        else {
+        } else {
             moveSpeed = walking;
             if (currentStamina < maxStamina) {
                 currentStamina++;
             }  
         }
-
-
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime); //fixedDeltaTime
+        
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
-    
 }
