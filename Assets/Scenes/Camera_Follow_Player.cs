1519234ec2122adc4f939ab0a1c7329a598caabc
Janko Hu <hu.janko@yahoo.de>
2022-05-03 18:55:34 +0200

    feat_12-Field small fixes

--- Assets/Scenes/Camera_Follow_Player.cs
+++ Assets/Scenes/Camera_Follow_Player.cs
@@ -6,20 +6,14 @@ public class Camera_Follow_Player : MonoBehaviour {
 
     public GameObject Player;
     public Vector3 offset = new Vector3(0,0,-1);
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
 
-    // Update is called once per frame
     void FixedUpdate() {
-        if(Player) {
-            transform.position = new Vector3(
+        if (Player) {
+            transform.position = new Vector3 (
                 Player.transform.position.x + offset.x,
                 Player.transform.position.y + offset.y,
-                Player.transform.position.z + offset.z);
-            
+                Player.transform.position.z + offset.z
+            );
         }
-        //transform.position = Player.transform.position + new Vector3(0, 1, -5);
     }
 }
